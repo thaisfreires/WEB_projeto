828364e9387ccb37dc383fe0170e395161a34401
Thais Freire <thaisfrsoares@gmail.com>
2024-07-27 18:31:00 +0100

    index.html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const carousel = new bootstrap.Carousel('#myCarousel')
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
